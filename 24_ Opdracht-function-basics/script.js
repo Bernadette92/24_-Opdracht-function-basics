5da6fb9dc50381076c984b7e076c67ac817224df
Bernadette92 <79193891+Bernadette92@users.noreply.github.com>
2021-03-14 15:15:29 +0100

    Add files via upload

---
+++ 24_ Opdracht-function-basics/script.js
@@ -0,0 +1,25 @@
+const makingPizza = function () {
+  console.log("heat oven");
+  console.log("make pizza dough");
+  console.log("cut vegatables");
+  console.log("spread tomato paste");
+  console.log("decorate pizza with vegatables");
+  console.log("put pizza in oven");
+  console.log("take pizza out of the oven");
+};
+
+const makingSushi = function () {
+  console.log("cook rice");
+  console.log("cut fish and vegatables");
+  console.log("take nori");
+  console.log("spread rice");
+  console.log("decorate rice with vegatables");
+  console.log("roll sushi");
+  console.log("cut sushi and serve");
+};
+
+makingSushi();
+makingSushi();
+makingSushi();
+makingPizza();
+makingPizza();
